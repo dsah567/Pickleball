52970b85ce0ae1501340ce361eccb225e9ec582f
Dipu Sah <sahd7929@gmail.com>
2024-10-17 15:53:33 +0545

    server setup verifyed

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+import express from "express"
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express()
+const allowedOrigins = "http://localhost:5173";
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // Allow credentials (cookies, authorization headers)
+}));
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(cookieParser())
+
+//importing routes
+// import teacherRouter from "";
+
+app.get("/",(req, res) => {
+    res.send('hello world')
+  })
+
+//route decleare
+// app.use(" ",)
+
+export {app}
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log("server is ready");
+import dotenv from "dotenv"
+import { app } from "./app.js";
+dotenv.config({
+    path: './.env'
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port,()=>{ console.log(`Server is listening at port ${port}`)} )
